64df5bcb4a8c413409c6839f5da8e85d3c58df08
abhinavprakashmishra12e-coder <abhinavprakashmishra12e@gmail.com>
2026-09-03 22:24:17 +0530

    03-09-2026

--- q_4.py
+++ q_4.py
@@ -1,2 +1,28 @@
 import speech_recognition as sr
 
+r=sr.Recognizer()
+
+with sr.Microphone() as source:
+    
+    audio=r.listen(source)
+text=r.recognize_google(audio)
+
+if text.lower() == "hello":
+    print("Hello! Nice to meet you.")
+
+elif text.lower() == "bye":
+    print("Goodbye!")
+
+else:
+    print("I don't understand.")
+
+try:
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        print("You said: " + text)
+except sr.UnknownValueError:
+    print("I could not understand the audio.")
+except sr.RequestError:
+    print("Could not request results from Google Speech Recognition service.")
